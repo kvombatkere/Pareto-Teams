420f9034852b7ef13eada69288f772ac6bc45517
Karan Vombatkere <kvombatkere@gmail.com>
2025-11-06 13:57:28 -0500

    graph diameter algo with sorted indices

--- paretoGraph.py
+++ paretoGraph.py
@@ -18,20 +18,95 @@ class paretoGraph():
         Initialize instance with n experts and single task
         Each expert and task consists of a list of skills
         ARGS:
-            task        : task to be accomplished;
-            n_experts   : list of n experts; each expert is a list of skills
-            costs       : cost of each expert
-            size_univ   : number of distinct skills in the universe
-            budget      : knapsack budget
+            task                : task to be accomplished;
+            n_experts           : list of n experts; each expert is a list of skills
+            pairwise_costs      : pairwise cost matrix between experts
+            size_univ           : number of distinct skills in the universe
+            budget              : knapsack budget
         '''
         self.task = task
         self.task_skills = set(task)
 
         self.experts = n_experts
         self.m, self.n = size_univ, len(self.experts)
-        self.pairwise_costs = pairwise_costs
+        self.pairwise_costs = np.asarray(pairwise_costs, dtype=float)
         self.B = budget
         logging.info("Initialized Pareto Coverage - Graph Cost Instance, Task:{}, Num Experts:{}, Budget={}".format(self.task, self.n, self.B))
 
 
-    
+    def greedyThresholdDiameter(self):
+        '''
+        Greedy procedure that grows a ball (by radius) around every expert (as center)
+        and tracks the best task coverage achievable at each radius.
+
+        Returns:
+            radii                : sorted 1D numpy array of radii considered (includes 0)
+            best_coverages       : list of best coverage values (same length as radii)
+            best_centers         : list of center expert indices that achieved the best coverage
+            best_included_lists  : list of lists of expert indices included for the best center at each radius
+            runtime              : elapsed time in seconds
+        '''
+        startTime = time.perf_counter()
+
+        #get unique pairwise costs
+        radii = np.insert(np.unique(self.pairwise_costs), 0, 0.0)  
+        best_coverages, best_centers, best_included_lists = [], [], []
+
+        #Maintain sets of experts centered at each expert
+        expertBalls = {i: set(self.experts[i]) for i in range(self.n)}
+        expertBallsIndices = {i: [i] for i in range(self.n)}
+
+        # Precompute per-center neighbor order and corresponding distances (increasing)
+        neighbor_order = np.argsort(self.pairwise_costs, axis=1, kind='mergesort')   # shape (n,n)
+        neighbor_dists = np.take_along_axis(self.pairwise_costs, neighbor_order, axis=1)  
+
+        r_prev = 0
+
+        # For each radius, evaluate ball around every expert and keep the best
+        for r in radii:
+            best_cov, best_center, best_included = -1, None, []
+
+            for center in range(self.n):
+                # find indices in the sorted neighbor_dists[center] that lie in (r_prev, r]
+                # prev_idx = count of distances <= r_prev; end_idx = count of distances <= r
+                prev_idx = np.searchsorted(neighbor_dists[center], r_prev, side='right')
+                end_idx = np.searchsorted(neighbor_dists[center], r, side='right')
+
+                # neighbors added at this step: those between prev_idx (exclusive) and end_idx (inclusive)
+                if end_idx > prev_idx:
+                    added_indices = neighbor_order[center, prev_idx:end_idx].tolist()
+                else:
+                    added_indices = []
+
+                # update ball sets and index lists incrementally
+                if added_indices:
+                    # union all skills of included experts
+                    expertBalls[center] = expertBalls[center].union(*(set(self.experts[j]) for j in added_indices))
+                    expertBallsIndices[center].extend(added_indices)
+
+                # compute coverage 
+                cov = (len(expertBalls[center] & self.task_skills) / len(self.task))
+
+                if cov > best_cov:
+                    best_cov = cov
+                    best_center = center
+                    best_included = expertBallsIndices[center]
+
+                # early stop if full coverage reached
+                if best_cov >= 1.0:
+                    break
+
+            best_coverages.append(best_cov)
+            best_centers.append(best_center)
+            best_included_lists.append(best_included)
+            r_prev = r #update previous radius
+            
+            # early stop if full coverage reached
+            if best_cov >= 1.0:
+                radii = radii[:len(best_coverages)]  # trim radii to current length
+                break
+
+        runTime = time.perf_counter() - startTime
+        logging.info("GreedyThresholdDiameter finished: max_coverage={:.3f}, runtime={:.3f}s".format(max(best_coverages) if best_coverages else 0.0, runTime))
+
+        return radii, best_coverages, best_centers, best_included_lists, runTime
--- utils.py
+++ utils.py
@@ -1,6 +1,35 @@
-import logging, pickle
+import logging, pickle, json
 import numpy as np
 logging.basicConfig(format='%(asctime)s |%(levelname)s: %(message)s', level=logging.INFO)
+
+
+#Import Datasets
+def importData(experts_filename, tasks_filename, numExperts=1000, exp_len=2, task_len=4):
+    with open(experts_filename, 'r') as f:
+        experts_list = json.loads(f.read())
+        expert_list_int = []
+        expert_index_list = []
+        for i, expert_skills in enumerate(experts_list[:numExperts]):
+            exp_skillset = sorted([int(skill) for skill in expert_skills])
+            #only keep unique experts with at least exp_len skills
+            if len(exp_skillset) >= exp_len and exp_skillset not in expert_list_int and len(expert_list_int) <= numExperts:
+                expert_list_int.append(exp_skillset)
+                expert_index_list.append(i)
+
+    with open(tasks_filename, 'r') as f:
+        tasks_list = json.loads(f.read())
+        task_list_int = []
+        for task_skills in tasks_list:
+            task_skills = sorted([int(skill) for skill in task_skills])
+            #Keep only unique tasks with at least task_len skills
+            if len(task_skills) >= task_len and task_skills not in task_list_int:
+                task_list_int.append(task_skills)
+
+    logging.info("Imported {} and {}. Num Experts={}, Num Tasks={}".format(experts_filename.split('/')[-1], tasks_filename.split('/')[-1],
+                                                                    len(expert_list_int),len(task_list_int)))
+    
+    return task_list_int, expert_list_int, expert_index_list
+
 
 def import_pickled_datasets(dataset_name, dataset_num):
     '''
@@ -21,8 +50,8 @@ def import_pickled_datasets(dataset_name, dataset_num):
         costs_arr = pickle.load(fp)
         logging.info("Imported {} costs, Num Costs: {}".format(dataset_name, len(costs_arr)))
 
-    # with open(data_path + 'graphMat_{}.pkl'.format(dataset_num), "rb") as fp:
-    #     graphmat = pickle.load(fp)
-    #     logging.info("Imported {} graph matrix, Shape: {}\n".format(dataset_name, graphmat.shape))
+    with open(data_path + 'graphMat_{}.pkl'.format(dataset_num), "rb") as fp:
+        graphmat = pickle.load(fp)
+        logging.info("Imported {} graph matrix, Shape: {}\n".format(dataset_name, graphmat.shape))
 
-    return experts, tasks, costs_arr
+    return experts, tasks, costs_arr, graphmat
